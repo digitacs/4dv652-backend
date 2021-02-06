24eb75ee24b53ea6c644d8852266c94c0bded532
Osama Zarraa <osamaz26@gmail.com>
2021-02-06 17:15:52 +0100

    chore: add djangorestframework feat: add scores app feat: add health app feat: connect to postgresql database feat(scores): add ScoreList api

--- 4dv652_backend/urls.py
+++ 4dv652_backend/urls.py
@@ -23,5 +23,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("hello.urls")),
+    path("health", include("health.urls")),
+    path("api/v1/scores", include("scores.urls")),
 ]
--- health/views.py
+++ health/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return HttpResponse("OK!")
