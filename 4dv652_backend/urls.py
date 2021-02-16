6ea4ce12462a5230eaa88d2939e41a91e98454be
Muhammad Farooq <raja.muhammad.farooq@gmail.com>
2021-02-16 19:48:18 +0100

    Update urls.py

--- 4dv652_backend/urls.py
+++ 4dv652_backend/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     #path('admin/', admin.site.urls),
     #path("api/v1/models/", include("models.regression.urls"))
-    path("api/v1/models/regression", include("regression.urls")),
+    path("health", include("health.urls")),
+    path("api/v1/scores", include("scores.urls")),
 ]
