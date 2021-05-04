519d3ed8f360d7299583c9336fc174b8c4be5be6
Muhammad Farooq <raja.muhammad.farooq@gmail.com>
2021-05-04 20:05:37 +0200

    DIS-138 #remove posenet

--- scores/functions.py
+++ scores/functions.py
@@ -1,10 +1,4 @@
-import tensorflow.compat.v1 as tf
-import posenet
-
 def handle_video_upload(f):  
     with open('media/video/'+f.name, 'wb+') as destination:  
         for chunk in f.chunks():  
             destination.write(chunk)
-
-def tf_version(): 
-    return tf.__version__
